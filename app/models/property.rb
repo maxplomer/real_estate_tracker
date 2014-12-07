c700c38a1839d532dfd6e286b1dcd253afbcb6e5
Max Plomer <maxplomer@gmail.com>
2014-12-07 15:39:33 -0500

    done with equations on dashboard, added link to equations.txt

--- app/models/property.rb
+++ app/models/property.rb
@@ -26,5 +26,40 @@ class Property < ActiveRecord::Base
     foreign_key: :property_id,
     dependent: :destroy
   )
+
+  def net_operating_income
+    self.income - self.expenses
+  end
+
+  def debt_service
+    truncate(self.interest_rate / 100 * self.loan_amount)
+  end
+
+  def cash_flow
+    net_operating_income - debt_service
+  end
+
+  def cash_on_cash
+    return "n/a" if down_payment == 0
+    truncate(cash_flow / down_payment * 100)
+  end
+
+  def cap_rate
+    truncate(net_operating_income / self.purchase_price)
+  end
+
+  def down_payment
+    self.purchase_price - self.loan_amount
+  end
+
+
+  private
+
+    def truncate(x)
+    (x * 100).floor / 100.0
+  end
+
+
+
 	
 end
