d3e707192a9bc8e22724ac972b29443d2b220186
Max Plomer <maxplomer@gmail.com>
2014-12-07 23:53:51 -0500

    patched weird error of properties going out of order

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class DashboardController < ApplicationController
   def show
   	if current_user.properties.length > 0
-      property_id = current_user.properties.first.id
+      property_id = current_user.properties.sort! { |a,b| b.id <=> a.id }.first.id
       redirect_to dashboard_url(property_id)
     else
       redirect_to new_property_url
--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,8 @@
 class DashboardsController < ApplicationController
 
   def show
-  	@properties = current_user.properties
+  	@properties = current_user.properties.sort! { |a,b| b.id <=> a.id }
+
     @property = Property.find(params[:id])
     render :show
   end
